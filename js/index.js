da82239ff5231bbc5a82ca95bc61fa483ff8d561
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-06-13 12:03:29 -0600

    tried to add parallax scroll

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
 "use strict";
-var swiper = new Swiper('.swiper-container', {
-        spaceBetween: 0,
-        centeredSlides: true,
-        loop: true,
-    autoplay: {
-        delay: 2750,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        },
-    });
 
 $(".backtotop").click(function() {
     return $("html, body").animate({
@@ -25,16 +8,11 @@ $(".backtotop").click(function() {
 $(".close").click(function(){
     $(".alert").slideUp();
 });
+$(window).scroll(function() {
+    var scrolledY = $(window).scrollTop();
+    $('.banner').css('background-position', 'left ' + ((scrolledY)) + 'px');
+});
 
 $(document).ready(function() {
     $(".alert").slideDown();
-    var mySwiper = document.querySelector('.swiper-container').swiper
-
-    $(".swiper-container").mouseenter(function() {
-        mySwiper.autoplay.stop();
-    });
-
-    $(".swiper-container").mouseleave(function() {
-        mySwiper.autoplay.start();
-    });
 });
---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+"use strict";
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+autoplay: {
+    delay: 2750,
+    disableOnInteraction: false,
+},
+pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+},
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+});
+
+$(document).ready(function() {
+    var mySwiper = document.querySelector('.swiper-container').swiper
+
+    $(".swiper-container").mouseenter(function() {
+        mySwiper.autoplay.stop();
+    });
+
+    $(".swiper-container").mouseleave(function() {
+        mySwiper.autoplay.start();
+    });
+});
